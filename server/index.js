fce0c87196cfac1c71d7aeb199aa4453ad9a9dc2
LevSarg3 <levimsargeant@gmail.com>
2020-04-17 16:59:46 -0600

    Just setting up the database at the time of this push

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 const express =require('express');
 const massive = require('massive')
 const app = express();
+const controller = require('./controller'
+)
 
-const port = 5340;
+const port = 4000;
+
+app.listen(port, function() {
+    console.log(`Port ${port} communication is on point!`)
+})
 
